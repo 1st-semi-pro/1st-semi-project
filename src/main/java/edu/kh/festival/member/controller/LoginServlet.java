5d6fe42c818b5db634b31c3381a45c446fb7835e
ohs <gudtjr1355@gmail.com>
2023-07-11 18:49:41 +0900

    커밋

--- src/main/java/edu/kh/festival/member/controller/LoginServlet.java
+++ src/main/java/edu/kh/festival/member/controller/LoginServlet.java
@@ -8,7 +8,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import edu.kh.festival.member.model.service.MemberService;
 import edu.kh.festival.member.model.vo.Member;
 
 @WebServlet("/member/login")
@@ -17,18 +19,46 @@ public class LoginServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-
 		String path = "/WEB-INF/views/member/login.jsp";
 		
 		req.getRequestDispatcher(path).forward(req, resp);
-		
 		
 	}
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+		// 로그인창에 아이디 입력하고 입력하고 로그인 누르면 값 담김
+		String inputId = req.getParameter("inputId");
+		String inputPw = req.getParameter("inputPw");
 		
+		// 확인용
+		System.out.println(inputId);
+		System.out.println(inputPw);
+		
+		// 입력한 아이디, 비번 Member VO에 세팅
+		Member mem = new Member();
+		mem.setMemberId(inputId);
+		mem.setMemberPw(inputPw);
+		
+		try {
+			
+				// 서비스 객체 만들기 (서비스 메소드 쓸라고)
+				MemberService service = new MemberService();
+				
+				// 이메일, 비번 일치하는 회원 있는지 조회하고 결과 반환받으러 가기
+				Member loginMember = service.login(mem);
+			
+				if(loginMember != null) {
+					System.out.println("성공");
+					
+				}else {
+					System.out.println("실패");
+				}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 		
 		
 	}
--- src/main/java/edu/kh/festival/member/model/dao/MemberDAO.java
+++ src/main/java/edu/kh/festival/member/model/dao/MemberDAO.java
@@ -1,5 +1,127 @@
 package edu.kh.festival.member.model.dao;
 
+import static edu.kh.festival.common.JDBCTemplate.*;
+
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Properties;
+
+import edu.kh.festival.member.model.vo.Member;
+
 public class MemberDAO {
 
-}
+	private Statement stmt;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	private Properties prop;
+		
+	// 기본 생성자
+	public MemberDAO() {
+		try {
+			prop = new Properties();
+			
+			String filePath =  MemberDAO.class.getResource("/edu/kh/festival/sql/member-sql.xml").getPath(); 
+			
+			prop.loadFromXML(new FileInputStream(filePath));
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+
+	/** 로그인 DAO
+	 * @param mem
+	 * @param conn
+	 * @return loginMember
+	 * @throws Exception
+	 */
+	public Member login(Member mem, Connection conn) throws Exception {
+		
+		Member loginMember = null; 
+		
+		try {
+			
+			String sql = prop.getProperty("login");
+			
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, mem.getMemberId());
+			pstmt.setString(2, mem.getMemberPw());
+			
+			System.out.println("mem.getMemberId:: " + mem.getMemberId());
+			System.out.println("mem.getMemberPw:: " + mem.getMemberPw());
+			
+			rs = pstmt.executeQuery();
+			
+			//System.out.println("rs.next():::" + rs.next());
+			
+			if(rs.next()) {
+				loginMember = new Member();
+				
+				loginMember.setMemberId(rs.getString("MEMBER_ID"));
+				loginMember.setMemberPw(rs.getString("MEMBER_PW"));
+				
+				System.out.println("loginMember rs:: " + loginMember);
+			}
+			
+			if(true) {
+				System.out.println("true입니다");
+			}
+			
+			
+		}finally {
+			close(rs);
+			close(pstmt);
+		}
+		
+		return loginMember;
+		
+	}
+
+	}		
+		
+/*		try {
+			
+			String sql = prop.getProperty("login");
+			
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, mem.getMemberId());
+			pstmt.setString(2, mem.getMemberPw());
+			
+			System.out.println("mem.getMemberId:: " + mem.getMemberId());
+			System.out.println("mem.getMemberPw:: " + mem.getMemberPw());
+			
+			rs = pstmt.executeQuery();
+			
+			System.out.println("rs.next():::" + rs.next());
+			
+			if(rs.next()){
+				loginMember = new Member();
+				
+				loginMember.setMemberId(rs.getString("MEMBER_ID"));
+				loginMember.setMemberPw(rs.getString("MEMBER_PW"));
+				
+				System.out.println("loginMember rs:: " + loginMember);
+			}
+			
+			if(true) {
+				System.out.println("true입니다");
+			}
+
+			
+		}finally {
+			close(rs);
+			close(pstmt);
+			
+		}
+		System.out.println("loginMember:: " + loginMember);
+		return loginMember;
+	}
+*/
+
