c35557ca4f8e02b1eefe55fa627eef2438ba3d1e
GWANG <mungwang1208@gmail.com>
2023-07-13 20:31:28 +0900

    07-13

--- src/main/webapp/resources/js/festival_information.js
+++ src/main/webapp/resources/js/festival_information.js
@@ -1,4 +1,8 @@
 const label = document.querySelectorAll('.label');
+
+const sp1 = document.getElementById("sp1");
+const sp2 = document.getElementById("sp2");
+
 label.forEach(function(lb){
     lb.addEventListener('click', e => {
         let optionList = lb.nextElementSibling;
@@ -6,24 +10,55 @@ label.forEach(function(lb){
         clickLabel(lb, optionItems);
     })
 });
+
 const clickLabel = (lb, optionItems) => {
     if(lb.parentNode.classList.contains('active')) {
         lb.parentNode.classList.remove('active');
         optionItems.forEach((opt) => {
             opt.removeEventListener('click', () => {
-                handleSelect(lb, opt)
+                handleSelect(lb, opt, sp1)
+                handleSelect1(lb, opt, sp2)
             })
         })
     } else {
         lb.parentNode.classList.add('active');
         optionItems.forEach((opt) => {
             opt.addEventListener('click', () => {
-                handleSelect(lb, opt)
+                handleSelect(lb, opt, sp1)
+                handleSelect1(lb, opt, sp2)
             })
         })
     }
 }
-const handleSelect = (label, item) => {
-    label.innerHTML = item.textContent;
+const handleSelect = (label, item, sp1) => {
+    sp1.innerHTML = item.textContent;
     label.parentNode.classList.remove('active');
 }
+const handleSelect1 = (label, item, sp2) => {
+    sp2.innerHTML = item.textContent;
+    label.parentNode.classList.remove('active');
+}
+
+/* ------------------------------------------------------------------------------- */
+
+new Swiper('.swiper-container', {
+
+	slidesPerView : 3, // 동시에 보여줄 슬라이드 갯수
+	spaceBetween : 30, // 슬라이드간 간격
+	slidesPerGroup : 3, // 그룹으로 묶을 수, slidesPerView 와 같은 값을 지정하는게 좋음
+
+	// 그룹수가 맞지 않을 경우 빈칸으로 메우기
+	// 3개가 나와야 되는데 1개만 있다면 2개는 빈칸으로 채워서 3개를 만듬
+	loopFillGroupWithBlank : true,
+
+	loop : true, // 무한 반복
+
+	pagination : { // 페이징
+		el : '.swiper-pagination',
+		clickable : true, // 페이징을 클릭하면 해당 영역으로 이동, 필요시 지정해 줘야 기능 작동
+	},
+	navigation : { // 네비게이션
+		nextEl : '.swiper-button-next', // 다음 버튼 클래스명
+		prevEl : '.swiper-button-prev', // 이번 버튼 클래스명
+	},
+});
