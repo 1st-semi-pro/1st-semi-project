b4a878f70bed3aaf378d95ca9f3022dae3fea9dc
GWANG <mungwang1208@gmail.com>
2023-07-27 19:09:38 +0900

    07-27

--- src/main/java/edu/kh/festival/board/model/dao/FestivalDAO.java
+++ src/main/java/edu/kh/festival/board/model/dao/FestivalDAO.java
@@ -3,6 +3,7 @@ package edu.kh.festival.board.model.dao;
 import static edu.kh.festival.common.JDBCTemplate.*;
 
 import java.io.FileInputStream;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
@@ -28,5 +29,67 @@ public class FestivalDAO {
 	         e.printStackTrace();
 	      }
 	   }
+
+	/** 게시판이름 조회 DAO
+	 * @param conn
+	 * @param type
+	 * @return boardName
+	 * @throws Exception
+	 */
+	public String selectBoardName(Connection conn, int type) throws Exception {
+
+		String boardName = null;
+
+		try {
+			String sql = prop.getProperty("BoardName");
+
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, type);
+
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+
+				boardName = rs.getString(1);
+			}
+
+		} finally {
+			close(rs);
+			close(stmt);
+		}
+
+		return boardName;
+	}
+
+	/** 축제게시글 조회 DAO
+	 * @param conn
+	 * @param type
+	 * @return festivalCount
+	 * @throws Exception
+	 */
+	public int getfestival(Connection conn, int type) throws Exception {
+		
+		int festivalCount = 0;
+
+		try {
+			String sql = prop.getProperty("festivalCount");
+
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, type);
+
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+
+				festivalCount = rs.getInt(1);
+			}
+
+		} finally {
+			close(rs);
+			close(stmt);
+		}
+
+		return festivalCount;
+	}
 
 }
--- src/main/java/edu/kh/festival/board/model/service/FestivalService.java
+++ src/main/java/edu/kh/festival/board/model/service/FestivalService.java
@@ -4,9 +4,12 @@ import static edu.kh.festival.common.JDBCTemplate.*;
 
 import java.sql.Connection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import edu.kh.festival.board.model.dao.FestivalDAO;
+import edu.kh.festival.board.model.vo.Festival;
+import edu.kh.festival.board.model.vo.Pagination9;
 
 public class FestivalService {
 	
@@ -15,12 +18,23 @@ public class FestivalService {
 	/** 축제정보 Service
 	 * @param type
 	 * @param cp
-	 * @return
-	 * @throws EnumConstantNotPresentException
+	 * @return map
+	 * @throws Exception
 	 */
-	public Map<String, Object> festivalInfo(int type, int cp) throws EnumConstantNotPresentException {
+	public Map<String, Object> festivalInfo(int type, int cp) throws Exception {
 		
 		Connection conn = getConnection();
+		
+		// 1. 게시판이름
+		String boardName = dao.selectBoardName(conn,type);
+		
+		// 2. 전체 축제 수 조회
+		int	festivalCount = dao.getfestival(conn,type);
+		
+		// 3. 페이지네이션
+		Pagination9 pagination = new Pagination9(cp, festivalCount);
+		// 3. 축제 목록조회
+		List<Festival> festival = dao.festivalList(conn,pagination,type);
 		
 		Map<String, Object> map = new HashMap<String, Object>();
 		return map;
